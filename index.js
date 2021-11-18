73eca85a74e981d33b28068fd03ab3e4cabdda70
Tom Taylor <hello@tomtaylor.name>
2021-11-18 14:28:53 +0000

    Refactor for ES Modules

--- index.js
+++ index.js
@@ -3,29 +3,24 @@
  * Runs the automated testing suite.
  * @param {String} --modules=[modules...] Comma separated list specifying which modules should be tested (all modules are tested by default).
  */
-const glob = require('glob');
-const Mocha = require('mocha');
-const path = require('path');
-const { App, Utils } = require('adapt-authoring-core');
+import { App } from 'adapt-authoring-core';
+import glob from 'glob';
+import Mocha from 'mocha';
 
 const TESTS_GLOB = 'tests/*.spec.js';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'testing';
 
 async function init() {
-  const pkg = await Utils.requirePackage();
-  console.log(`Running test suite for ${pkg.name}@${pkg.version}\n`);
-
-  await App.instance.onReady();
-
+  // store global data for use by test suite
+  global.ADAPT = { app: await App.instance.onReady() };
+  console.log(`Running test suite for ${global.ADAPT.app.name}@${global.ADAPT.app.version}\n`);
   const testFiles = getTestFiles();
 
   if(!testFiles.length) {
     console.log('No tests defined!');
     process.exit();
   }
-  setGlobalData();
-
   const mocha = new Mocha();
   mocha.files.push(...testFiles);
   try {
@@ -39,54 +34,16 @@ async function init() {
  * Lists test specs to be run
  */
 function getTestFiles() {
-  return getModulesForTesting().reduce((m,d) => {
-    const tests = glob.sync(TESTS_GLOB, { cwd: Utils.getModuleDir(d), realpath: true });
-    if(!tests.length) return m;
-    console.log(`Tests defined for '${d}'`);
-    return m.concat(tests);
+  const whitelist = App.instance.args?.modules.split(',') ?? [];
+  const includedModules = Object.values(App.instance.dependencies)
+    .filter(d => !whitelist.length || whitelist.includes(d.name) || whitelist.includes(`adapt-authoring-${d.name}`));
+
+  return includedModules.reduce((modules,mod) => {
+    const tests = glob.sync(TESTS_GLOB, { cwd: mod.rootDir, realpath: true });
+    if(!tests.length) return modules;
+    console.log(`Tests defined for '${mod.name}'`);
+    return modules.concat(tests);
   }, []);
-}
-/**
- * Returns the list of modules which should be tested
- * if --modules= is passed, only specified modules will be tested
- */
-function getModulesForTesting() {
-  const includedModules = App.instance.args.modules;
-  const allDeps = Object.keys(App.instance.dependencies);
-  if(!includedModules) {
-    return allDeps;
-  }
-  return includedModules.split(',').reduce((a,m) => {
-    const mLong = `adapt-authoring-${m}`;
-    if(allDeps.includes(m)) a.push(m);
-    else if(allDeps.includes(mLong)) a.push(mLong);
-    return a;
-  }, []);
-}
-/**
- * Mocks the App instance for use in tests
- */
-function setGlobalData() {
-  const config = generateConfigData();
-  global.ADAPT = {
-    app: {
-      dependencies: [],
-      dependencyloader: { modules: {}, utilities: {}, on: () => {} },
-      lang: { t: k => k },
-      config: { config, get: key => config[key] || key },
-      onReady: () => Promise.resolve()
-    }
-  };
-}
-/**
- * Loads the testing config data
- */
-function generateConfigData() {
-  const configPath = path.join(process.cwd(), 'conf', `${process.env.NODE_ENV}.config.js`);
-  return Object.entries(require(configPath)).reduce((m,[d,c]) => {
-    Object.entries(c).forEach(([k,v]) => m[`${d}.${k}`] = v);
-    return m;
-  },{});
 }
 
-module.exports = init();
+export default init();
